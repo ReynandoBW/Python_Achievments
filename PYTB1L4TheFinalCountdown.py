7056fd730b6a9f4e98eb9967ad0b6ec0a60e68d4
ReynandoBW <71132492+ReynandoBW@users.noreply.github.com>
2020-10-01 13:00:36 +0200

    Add files via upload

---
+++ PYTB1L4TheFinalCountdown.py
@@ -0,0 +1,9 @@
+running = True
+number = 1001
+while (running == True):
+    number -= 1
+    print(number)
+    if (number == 0):
+        running = False
+
+print("https://www.youtube.com/watch?v=RINHjxsPOEg")
