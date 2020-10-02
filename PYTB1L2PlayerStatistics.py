b64b55017fda7a4bb2606e367b8360da770299fc
ReynandoBW <71132492+ReynandoBW@users.noreply.github.com>
2020-10-02 14:30:30 +0200

    Add files via upload

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,21 @@
+Name  = "Reynando"
+Age   = 17
+Race  = "Human"
+Str   = 9
+Dex   = 14
+Con   = 13
+Int   = 16
+Wis   = 13
+Cha   = 14
+Money = 825.49
+
+print("Name         :", Name)
+print("Age          :", Age)
+print("Race         :", Race)
+print("Strength     :", Str)
+print("Dexterity    :", Dex)
+print("Constitution :", Con)
+print("Intelligence :", Int)
+print("Wisdom       :", Wis)
+print("Charisma     :", Cha)
+print("Money        : $", Money)
