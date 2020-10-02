642d36ee986196c278779a89ab487585bc04e2bd
ReynandoBW <71132492+ReynandoBW@users.noreply.github.com>
2020-10-02 14:13:45 +0200

    Add files via upload

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,7 @@
+vers1 = "Reynando is my name. Everyone knows that. You didn't? Now you do! Anime watching is one of my hobby's. And reading is another one. Now, this poem is comming to an end. Do remember me until the end. Or you'll regret it."
+
+#oke, dus, dit is een leuk versje die ik samen met een collega van mij heb bedacht.
+vers2 = "Reynando, Reynando. Zullen wij eten zo? Ik heb trek, in een snack. Fuck het bier is weer lek"
+print(vers1)
+print("-----------------------------------------------------------------------------")
+print(vers2)
