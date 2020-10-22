8a5bf4334e3cca4aa96697f26ff02d228805095c
ReynandoBW <71132492+ReynandoBW@users.noreply.github.com>
2020-10-22 11:43:09 +0200

    Add files via upload

---
+++ PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,14 @@
+lijst = ["een", 2, True, "Hallo", False]
+print(lijst)
+print("-----------------------------------")
+
+lijst.insert(3, "Yeet")
+print(lijst)
+print("-----------------------------------")
+
+lijst.pop(5)
+print(lijst)
+print("-----------------------------------")
+
+lijst.reverse()
+print(lijst)
